1e1c3ded94a21ae20327ce5fbde00ee38b1c6fea
Moheax <Maxvanlierop05@gmail.com>
2020-10-19 23:53:32 +0200

    Commit

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,6 +1,13 @@
+
+# list of employees
 employees = ("Max","Naru","Smol","Ginger","Devil","Bitz","Bear","Ruka","Khael","LRavellie")
 
+# sorts employees alphabetical
 Sortedemployees = sorted(employees)
 
+# Prints employee list in alphabetical order
 print(Sortedemployees)
+
+# prints amount of employees
 print("employees =",len(Sortedemployees))
+
