8638f4f142cdf07fa8fec3cd0cee425101977edf
Moheax <Maxvanlierop05@gmail.com>
2020-10-21 01:09:01 +0200

    Commit

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,5 +1,13 @@
-employees = ("Max","Naru","Smol","Ginger","Devil","Bitz","Bear","Ruka","Khael","LRavellie")
+employees = (("Max", 500), ("Naru", 500), ("Smol", 500), ("Ginger", 500), ("Devil", 500), ("Bitz", 500), ("Bear", 500), ("Ruka", 500), ("Khael", 500), ("LRavellie", 500))
 
 employeesdict = dict(employees)
-
+# prints dict with name and salary
 print(employeesdict)
+print("--------------")
+
+employees = ("Max","Naru","Smol","Ginger","Devil","Bitz","Bear","Ruka","Khael","LRavellie")
+salary = (500)
+print(employees)
+print(salary)
+
+print("--------------")
